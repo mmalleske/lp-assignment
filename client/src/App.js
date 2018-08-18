6e2ba2be59c42cc1990139eb16df0b1e03a5a183
Michael Malleske <mmalleske@gmail.com>
2018-08-18 15:35:32 -0500

    loads profiles from db

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo'
+
+const ProfilesQuery = gql`
+  {
+  	profiles {
+      name
+      id
+    }
+  }
+`;
 
 class App extends Component {
   render() {
+    const {data: {loading, profiles}} = this.props;
+    console.log(this.props)
+    if(loading) {
+      return (
+        <div>Loading...</div>
+      );
+    }
+
     return (
-      <div className="App">
-        <div>hello</div>
-      </div>
+      <div>{profiles.map((profile, key)=> (
+        <div key={profile.id}>{profile.name}</div>
+      ))}</div>
     );
   }
 }
 
-export default App;
+export default graphql(ProfilesQuery)(App);
